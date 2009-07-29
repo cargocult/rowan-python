a6f19e50711e0224102c3fe54f13f136181eff73
idmillington <idmillington@googlemail.com>
2009-07-29 14:37:35 +0100

    Added github tools to sphinx to avoid underscore in generated docs.

--- doc/conf.py
+++ doc/conf.py
@@ -27,7 +27,10 @@ sys.path.append('..') # So we can run the make file and have it find the code.
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.coverage']
+extensions = [
+    'sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.coverage',
+    'github.tools.sphinx'
+    ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = []
